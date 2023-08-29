46b7dd9de111f3ad62e399ea3842116db2200b8c
ddeshya <uddeshyasingh1111@gmail.com>
2023-08-29 04:45:22 -0700

    ggg

---
+++ Subtraction.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Subtraction{
+    public static void main(String [] args){
+    Scanner input = new Scanner (System.in);
+    System.out.println("Enter value of a ");
+    int a = input.nextInt();
+    System.out.println("Enter Value of b");
+    int b =input.nextInt();
+    int c;
+    c= a-b;
+System.out.println("value of c is " + c) ;
+    }
+}
